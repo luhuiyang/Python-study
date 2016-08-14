20427cbd0fa211b1987e79abce11efaac6da4561
luhuiyang <ylhyajayaj@gmail.com>
2016-08-15 02:05:15 +0800

    课2作业部分

--- WebProjects/课2/homework2/课2作业.py
+++ WebProjects/课2/homework2/课2作业.py
@@ -17,6 +17,10 @@ def log(*args, **kwargs):
     print(*args, **kwargs)
 
 
+import ssl
+import socket
+
+
 # 作业 2.1
 #
 # 实现函数
@@ -28,7 +32,13 @@ def path_with_query(path, query):
     返回一个拼接后的 url
     详情请看下方测试函数
     '''
-    pass
+    q = ''
+    for k, v in query.items():
+        q += str(k) + '=' + str(v) + '&'
+        log('query', q)
+    log('path and query', path + '?' + q[:-1])
+
+    return path + '?' + q[:-1]
 
 
 def test_path_with_query():
@@ -43,13 +53,128 @@ def test_path_with_query():
         '/?height=169&name=gua',
     ]
     # NOTE, 字典是无序的, 不知道哪个参数在前面, 所以这样测试
-    assert path_with_query(path, query) in expected
+    assert path_with_query(path, query) in expected, '错误'
+
+
+test_path_with_query()
+
+
+# 课1作业的部分
+def parsed_url(url):
+    """
+    解析 url 返回 (protocol host port path)
+    有的时候有的函数, 它本身就美不起来, 你要做的就是老老实实写
+    """
+    # 检查协议
+    protocol = 'http'
+    if url[:7] == 'http://':
+        u = url.split('://')[1]
+    elif url[:8] == 'https://':
+        protocol = 'https'
+        u = url.split('://')[1]
+    else:
+        # '://' 定位 然后取第一个 / 的位置来切片
+        u = url
+
+    # 检查默认 path
+    i = u.find('/')
+    if i == -1:
+        host = u
+        path = '/'
+    else:
+        host = u[:i]
+        path = u[i:]
+
+    # 检查端口
+    port_dict = {
+        'http': 80,
+        'https': 443,
+    }
+    # 默认端口
+    port = port_dict[protocol]
+    if host.find(':') != -1:
+        h = host.split(':')
+        host = h[0]
+        port = int(h[1])
+
+    return protocol, host, port, path
+
+
+def socket_by_protocol(protocol):
+    """
+    根据协议返回一个 socket 实例
+    """
+    if protocol == 'http':
+        s = socket.socket()
+    else:
+        # HTTPS 协议需要使用 ssl.wrap_socket 包装一下原始的 socket
+        # 除此之外无其他差别
+        s = ssl.wrap_socket(socket.socket())
+    return s
+
+
+def response_by_socket(s):
+    """
+    参数是一个 socket 实例
+    返回这个 socket 读取的所有数据
+    """
+    response = b''
+    buffer_size = 1024
+    while True:
+        r = s.recv(buffer_size)
+        if len(r) == 0:
+            break
+        response += r
+    return response
+
+
+def parsed_response(r):
+    """
+    把 response 解析出 状态码 headers body 返回
+    状态码是 int
+    headers 是 dict
+    body 是 str
+    """
+    header, body = r.split('\r\n\r\n', 1)
+    h = header.split('\r\n')
+    status_code = h[0].split()[1]
+    status_code = int(status_code)
+
+    headers = {}
+    for line in h[1:]:
+        k, v = line.split(': ')
+        headers[k] = v
+    return status_code, headers, body
 
 
 # 作业 2.2
 #
 # 为作业1 的 get 函数增加一个参数 query
 # query 是字典
+def get(url, query):
+    """
+    用 GET 请求 url 并返回响应
+    """
+    protocol, host, port, path = parsed_url(url)
+    if query != None:
+        path = path_with_query(path, query)
+
+    s = socket_by_protocol(protocol)
+    s.connect((host, port))
+
+    request = 'GET {} HTTP/1.1\r\nhost: {}\r\nConnection: close\r\n\r\n'.format(path, host)
+    encoding = 'utf-8'
+    s.send(request.encode(encoding))
+
+    response = response_by_socket(s)
+    r = response.decode(encoding)
+
+    status_code, headers, body = parsed_response(r)
+    if status_code == 301:
+        url = headers['Location']
+        return get(url)
+
+    return status_code, headers, body
 
 
 # 作业 2.3
@@ -67,18 +192,100 @@ def header_from_dict(headers):
     返回如下 str
     'Content-Type: text/html\r\nContent-Length: 127\r\n'
     '''
-    pass
+    d = ''
+    for k, v in headers.items():
+        d += str(k) + ': ' + str(v) + '\r\n'
+    log('dictStr', d)
+    return d
+
 
 # 作业 2.4
 #
 # 为作业 2.3 写测试
+def test_header_from_dict():
+    # 注意 height 是一个数字
+    headers = {
+        'Content-Type': 'text/html',
+        'Content-Length': 127,
+    }
+    expected = [
+        'Content-Type: text/html\r\nContent-Length: 127\r\n'
+        'Content-Length: 127\r\nContent-Type: text/html\r\n'
+    ]
+    # NOTE, 字典是无序的, 不知道哪个参数在前面, 所以这样测试
+    log('header_from_dict', header_from_dict(headers))
+    assert header_from_dict(headers) in expected
 
 
+# test_header_from_dict()
+
+# 作业 2.5
+#
+"""
+豆瓣电影 Top250 页面链接如下
+https://movie.douban.com/top250
+我们的 client 已经可以获取 https 的内容了
+这页一共有 25 个条目
+
+所以现在的程序就只剩下了解析 HTML
+
+请观察页面的规律，解析出
+1，电影名
+2，分数
+3，评价人数
+4，引用语（比如第一部肖申克的救赎中的「希望让人自由。」）
+
+解析方式可以用任意手段，如果你没有想法，用字符串查找匹配比较好
+"""
 
 
+def get_detail(result, is_start=False):
+    begin_position = 0
+
+    is_start = not is_start
+
+    if is_start:
+        start = result.find('"title">')
+        name = ''
+        result = result[start + 20:]
+        for s in result:
+            if s != '<':
+                name += s;
+            else :
+                log('movie name', name)
+                break
+        begin_position = len(name)
+    else:
+        begin_position = 27
+    get_detail(result[begin_position:], is_start)
+    pass
+
+status_code, headers, body = get('https://movie.douban.com/top250', None)
+log('movie_data', body)
+get_detail(body)
 
 
+# 作业 2.6
+#
+"""
+通过在浏览器页面中访问 豆瓣电影 top250 可以发现
+1, 每页 25 个条目
+2, 下一页的 URL 如下
+https://movie.douban.com/top250?start=25
 
+因此可以用循环爬出豆瓣 top250 的所有网页
 
+于是就有了豆瓣电影 top250 的所有网页
 
+由于这 10 个页面都是一样的结构，所以我们只要能解析其中一个页面就能循环得到所有信息
 
+所以现在的程序就只剩下了解析 HTML
+
+请观察规律，解析出
+1，电影名
+2，分数
+3，评价人数
+4，引用语（比如第一部肖申克的救赎中的「希望让人自由。」）
+
+解析方式可以用任意手段，如果你没有想法，用字符串查找匹配比较好
+"""
